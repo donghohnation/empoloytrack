896f8d62ace6f416cef0d6abd1a66f3cb97d2a6c
donghohnation <donghoh.han@gmail.com>
2022-07-10 18:08:12 -0700

    updated js

--- server.js
+++ server.js
@@ -1,1 +1,167 @@
-const db = mysql.createConnection('mysql://root:chickenrecipe123@localhost:3306/employeetr_db')
+const mysql = require('mysql2');
+const db = mysql.createConnection('mysql://root:chickenrecipe123@localhost:3306/employeetr_db');
+const inquirer = require('inquirer');
+
+
+
+const question = () => {
+inquirer.prompt([{
+  message: 'what would you like to do',
+  type: 'list',
+  choices: ['add department', 'add role', 'add employee', 'view departments', 'view roles', 'view employees', 'update employee', 'nada'],
+  name: 'choice'
+}])
+.then(initial => {
+  console.log(initial)
+  console.log(initial.choice)
+  switch(initial.choice) {
+    case 'add department':
+    addDepartment()
+    break
+    case 'add role':
+    addRole()
+    break
+    case 'add employee':
+    addEmployee()
+    break
+    case 'view departments':
+    viewDepartments()
+    break
+    case 'view roles':
+    viewRoles()
+    break
+    case 'view employees':
+    viewEmployees()
+    break
+    case 'update employee':
+    updateEmployee()
+    break
+    case 'nada':
+    console.log('nothingness...')
+    break
+  }
+})
+}
+
+const addDepartment = () => {
+  console.log('department addition attempt!')
+  inquirer.prompt([{
+    message: 'what department are you adding',
+    type: 'input',
+    name: 'name'
+  }])
+  .then(department => {
+    console.log(department)
+    db.query('INSERT INTO departments SET ?', department, err =>{
+      if(err) {console.log(err)}
+    })
+    console.log('department now added!')
+    question()
+  })
+}
+
+const addRole = () => {
+  console.log('role addition attempt!')
+  inquirer.prompt([{
+    message: 'what role title are you adding',
+    type: 'input',
+    name: 'title'
+  },
+  {
+    message: 'what is the salary for the role',
+    type: 'input',
+    name: 'salary'
+  },
+  {
+    message: 'what is the department ID',
+    type: 'input',
+    name: 'department_id'
+  }
+])
+  .then(role => {
+    console.log(role)
+    db.query('INSERT INTO roles SET ?', role, err =>{
+      if(err) {console.log(err)}
+    })
+    console.log('role now added!')
+    question()
+  })
+}
+
+const addEmployee = () => {
+  console.log('employee addition attempt!')
+  inquirer.prompt([{
+    message: 'what employee first name are you adding',
+    type: 'input',
+    name: 'firstname'
+  },
+  {
+    message: 'what employee last name are you adding',
+    type: 'input',
+    name: 'lastname'
+  },
+  {
+    message: 'what is the role ID',
+    type: 'input',
+    name: 'role_id'
+  },
+  {
+    message: 'is the employee a manager',
+    type: 'list',
+    choices: ['yes', 'no'],
+    name: 'managerBoolean'
+  }
+  ])
+  .then(employee => {
+    console.log(employee)
+    if(employee.managerBoolean === 'yes') {
+      console.log('MANAGER addition attempt!')
+      delete employee.managerBoolean
+      console.log(employee)
+      db.query('INSERT INTO employees SET ?', employee, err => {
+        if (err) { console.log(err) }
+      })
+      console.log('employee now added!')
+      question()
+    }
+
+    else if(employee.managerBoolean === 'no') {
+      console.log('nonMANAGER addition attempt!')
+      inquirer.prompt([{
+        message: 'what is the id of the manager of employee',
+        type: 'input',
+        name: 'manager_id'
+      }])
+      .then(nonmanager => {
+        console.log(nonmanager)
+
+        delete employee.managerBoolean
+        let newEmployee = {
+          ...employee,
+          ...nonmanager
+        }
+        db.query('INSERT INTO employees SET ?', newEmployee, err => {
+          if (err) { console.log(err) }
+        // db.query('INSERT INTO employees SET ?', employee, err => {
+        //   if (err) { console.log(err) }
+        })
+        console.log('employee now added!')
+        question()
+        })
+    }
+  })
+}
+
+const updateRole = () =>{
+  inquirer.prompt([{
+    message: 'what is the id of the employee you want to update',
+    type: 'input',
+    name: 'id'
+  }])
+  .then(employee =>{
+    
+  })
+}
+
+
+question()
